30242b19d514e466f65bcc0683190f7fe2ef5b81
peyoot <peyoot@hotmail.com>
2020-12-01 03:51:21 -0600

    fix diskusage sytax issue

--- server-maintainer.sh
+++ server-maintainer.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+## To use this script, please put sendemail.sh in /usr/bin
+## any issue please report to peyoot#hotmail.com 
+## check if bc package have been installed, if not simply sudo apt install bc
 ##
 ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}')
-diskusage=$(df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB %s\n", $3,$2,$5}' | awk '{print $4}') | awk -F'%' '{print $1}'
+diskusage=$(df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB %s\n", $3,$2,$5}' | awk '{print $4}' | awk -F'%' '{print $1}')
 cpuusage=$(top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' | awk '{print $3/4}')
 #上面内存占比不带%,diskusage直接带%了,cpu占比则以小数表示。
 ALARMBODY="CPU/Memory/Disk Utilization"
@@ -29,7 +32,7 @@ echo "$ALARMBODY" >> $MESSAGE
 echo "" >> $MESSAGE
 echo "------------------------------------------------------------------" >> $MESSAGE
 
-/home/root/tools/sendemail.sh peyoot@hotmail.com "$TO" "$SUBJECT" ""$MESSAGE""
+/usr/bin/sendemail.sh "$TO" "$SUBJECT" ""$MESSAGE""
 rm /tmp/Mail.out
 
 #######每周备份，内容生成到/var/log/weekly.report中#######
