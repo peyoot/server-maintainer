22ffe03c6ff75489327f6e6736a1337cc65d7f67
peyoot <peyoot@hotmail.com>
2020-12-01 04:04:02 -0600

    add backup docker wekan-db, postmysql

--- server-maintainer.sh
+++ server-maintainer.sh
@@ -3,10 +3,11 @@
 ## any issue please report to peyoot#hotmail.com 
 ## check if bc package have been installed, if not simply sudo apt install bc
 ##
+
+DATE=$(date +%Y-%m-%d-%H-%M)
 ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}')
 diskusage=$(df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB %s\n", $3,$2,$5}' | awk '{print $4}' | awk -F'%' '{print $1}')
 cpuusage=$(top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' | awk '{print $3/4}')
-#上面内存占比不带%,diskusage直接带%了,cpu占比则以小数表示。
 ALARMBODY="CPU/Memory/Disk Utilization"
 if [ $(echo "$ramusage > 80"|bc) -eq 1 ]; then
   echo "Memory Alarm: reach ${ramusage}% utilization" >> $ALARMBODY
@@ -25,7 +26,7 @@ fi
 
 
 #####
-SUBJECT="Server $(hostname) maintainer report: $(date +%Y-%m-%d-%H-%M)"
+SUBJECT="$DATE Server $(hostname) maintainer report"
 MESSAGE="/tmp/Mail.out"
 TO="peyoot@hotmail.com"
 echo "$ALARMBODY" >> $MESSAGE
@@ -36,4 +37,27 @@ echo "------------------------------------------------------------------" >> $ME
 rm /tmp/Mail.out
 
 #######每周备份，内容生成到/var/log/weekly.report中#######
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH
+mkdir -p backups/$DATE
 
+#backup code here
+#backup docker mysql8
+
+
+#backup docker mysql5
+
+
+#backup docker wenkan-db postgresql
+docker ps -a | grep 'wekan-db' &> /dev/null
+if [ $? = 0 ]; then
+  docker exec -t wekan-db bash -c "rm -fr /dump ; mkdir /dump ; mongodump -o /dump/"
+  docker cp wekan-db:/dump $SCRIPTPATH/backups/$DATE
+  tar -zc -f backups/$DATE.tgz -C $SCRIPTPATH/backups/$DATE wekan
+  if [ -f backups/$DATE.tgz ]; then
+    rm -fr backups/$DATE
+    find $SCRIPTPATH/backups/ -name "*.tgz" -mtime +7 -delete
+  fi 
+else
+  echo "wekan-db container is not running" >> $MESSAGE
+fi
