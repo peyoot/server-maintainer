215fdd39564c914323d911a39fbccfb41bf19497
peyoot <peyoot@hotmail.com>
2023-02-26 22:09:29 -0600

    update varials checking and something, not test yet

--- server-maintainer.sh
+++ server-maintainer.sh
@@ -14,6 +14,7 @@ has_root
 
 ####BLOCK1: check the availability of necessary packages and install missing dependencies####
 #check os
+HOSTNAME=$(hostname)
 OS_RELEASE=$(awk -F= '/^NAME/{gsub(/"/, "", $2);print $2}' /etc/os-release)
 if [[ "$OS_RELEASE" =~ "Ubuntu" ]] || [[ "OS_REALSE" =~ "Debian" ]]; then
   PACKAGE_UPDATE="sudo apt update"
@@ -77,6 +78,11 @@ if [[ ! ${variables[@]} =~ "SMTP_PASSWORD" ]]; then
   echo "missing key variables. Abort"
   exit
 fi
+if [[ ! ${variables[@]} =~ "EMAIL_TO" ]]; then
+  echo "missing key variables. Abort"
+  exit
+fi
+
 
 printf "total %s variables in env file and they are:\n" $arr_length
 echo "${variables[@]}"
@@ -87,7 +93,7 @@ for v in "${variables[@]}"; do
 done
 ####End of BLOCK2####
 
-
+####BLOCK3: check system status and prepare Email harder
 DATE=$(date +%Y-%m-%d-%H-%M)
 ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}')
 diskusage=$(df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB %s\n", $3,$2,$5}' | awk '{print $4}' | awk -F'%' '{print $1}')
@@ -103,17 +109,26 @@ if [ $(echo "$cpuusage > 0.7"|bc) -eq 1 ]; then
   echo "CPU Alarm: reach 70% utilization <br />" >> $ALARMBODY
 fi
 
-####Email notification header
+#Email notification header
 
-SUBJECT="$DATE Server $(hostname) maintainer report"
+SUBJECT="$DATE Server ${HOSTNAME} maintainer report"
 MESSAGE="/tmp/Mail.out"
 TO="peyoot@hotmail.com"
 echo "$ALARMBODY <br />" >> $MESSAGE
 echo "" >> $MESSAGE
 echo "------------------------------------------------------------------<br /> " >> $MESSAGE
+####End of BLOCK3####
 
+####BLOCK4: portainer backup####
+TOKEN=$(curl -s -X POST -H 'Accept: application/json' -H 'Content-Type: application/json' --data '{"username":"${PORTAINER_ADMIN}","password":"${PORTAINER_PASSWORD}"}' "${PORTAINER_URL}"/api/auth | jq -r '.jwt')
 
-#### database backup####
+curl --request POST \
+--url http://172.100.100.1:9000/api/backup \
+--header 'Authorization: Bearer ${TOKEN}' \
+--header 'Content-Type: application/json' > ${HOSTNAME}-portainer-backup.tar.gz
+####End of Block4####
+
+####Block5:  database backup####
 #echo script log to message for email report
 #put database backup here
 #######每周备份，内容生成到/var/log/weekly.report中#######
@@ -168,7 +183,7 @@ fi
 echo "all availabe backup have been packed" >> $MESSAGE
 
 
-
+####End of Block5####
 ##### call sendemail
 
 /usr/local/bin/sendemail.sh "$TO" "$SUBJECT" "$MESSAGE"
